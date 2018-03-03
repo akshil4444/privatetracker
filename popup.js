80a0e61662708a74800e0bdc8ef314ffbe398916
akshil <akshilshah4444@gmail.com>
2018-03-03 16:29:32 +0530

    working for the four website

---
+++ popup.js
@@ -0,0 +1,9 @@
+document.getElementById('submit').addEventListener('click',function(){
+
+    var input1= document.getElementById('input1').value;
+    chrome.tabs.create({url: "https://www.torrentleech.org/torrents/browse/index/query/" + input1 + "/newfilter"});
+	  chrome.tabs.create({url:"https://torrentbytes.net/browse.php?search=" + input1 + "&cat=0&sc=1"});
+	  chrome.tabs.create({url:"https://privatehd.to/torrents?in=1&search=" +input1 + "&type=0"});
+	  chrome.tabs.create({url:"https://uhdbits.org/torrents.php?searchstr=" +input1 + "&type=0"});
+    
+});
